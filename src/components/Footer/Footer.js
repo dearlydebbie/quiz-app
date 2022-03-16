3c2e9620a4b9a89656c2f242dddb8b93247d2d28
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-03-16 13:09:32 +0100

    quiz app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+		<div style={{textAlign: 'center', marginBottom: 10}}>
+			<a
+				href="https://github.com/dearlydebbie"
+				target="_blank"
+				rel="noopener noreferrer" style={{cursor: "pointer"}}
+			>
+				Open-Source Code{" "}
+			</a>
+			by Deborah Adeoye
+		</div>
+	);
+}
+
+export default Footer
